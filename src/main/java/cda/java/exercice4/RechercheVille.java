36f2fef0bd427072d49122297b15cd2b19729f03
Iradium59 <samuel.definance003.jeu@gmail.com>
2024-10-17 10:23:15 +0200

    test 2 fonctionnel

--- src/main/java/cda/java/exercice4/RechercheVille.java
+++ src/main/java/cda/java/exercice4/RechercheVille.java
@@ -4,11 +4,25 @@ import cda.java.exception.NotFoundException;
 
 import java.util.List;
 
-public class RechercheVille {
-    private List<String> villes;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    public List<String> rechercher(String mot)  {
-        throw new NotFoundException();
-//        return null;
+public class RechercheVille {
+
+    private List<String> villes = List.of(
+            "Paris", "Budapest", "Skopje", "Rotterdam", "Valence", "Vancouver",
+            "Amsterdam", "Vienne", "Sydney", "New York", "Londres", "Bangkok",
+            "Hong Kong", "Dubaï", "Rome", "Istanbul"
+    );
+
+    public List<String> rechercher(String mot) throws NotFoundException {
+        if (mot.length() < 2) {
+            throw new NotFoundException();
+        }
+
+        return villes.stream()
+                .filter(ville -> ville.startsWith(mot))
+                .collect(Collectors.toList());
     }
 }
+
