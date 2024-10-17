7990f64ceb9b91e80a978022424956299f6531b1
Iradium59 <samuel.definance003.jeu@gmail.com>
2024-10-17 10:59:41 +0200

    creation test non fonctionnel

---
+++ src/main/java/cda/java/exercice5/Shop.java
@@ -0,0 +1,11 @@
+package cda.java.exercice5;
+
+import java.util.List;
+
+public class Shop {
+    private List<Product> products;
+
+    public void update(Product product) {
+       return;
+    }
+}
