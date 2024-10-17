88e3ed81099cdedc6c9aa42c316265a9ed05111c
Iradium59 <samuel.definance003.jeu@gmail.com>
2024-10-17 11:07:20 +0200

    test 3 fonctionnel

--- src/main/java/cda/java/exercice5/Shop.java
+++ src/main/java/cda/java/exercice5/Shop.java
@@ -8,8 +8,16 @@ public class Shop {
     public void update(Product product) {
         product.setSellIn(product.getSellIn() - 1);
 
-        if (product.getQuality() > 0) {
-            product.setQuality(product.getQuality() - 1);
+        if (product.getSellIn() < 0) {
+            if (product.getQuality() > 1) {
+                product.setQuality(product.getQuality() - 2);
+            } else {
+                product.setQuality(0);
+            }
+        } else {
+            if (product.getQuality() > 0) {
+                product.setQuality(product.getQuality() - 1);
+            }
         }
     }
 }
