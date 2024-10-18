6fc33f1eb42f8678927b1ee52e4dc908a5589462
Iradium59 <samuel.definance003.jeu@gmail.com>
2024-10-18 11:43:21 +0200

    ajout and

--- src/test/java/cda/java/exercice7/ValidOrder.java
+++ src/test/java/cda/java/exercice7/ValidOrder.java
@@ -19,6 +19,11 @@ public class ValidOrder {
         System.out.println("Confirmation : La commande a été validée.");
     }
 
+    @Then("l'utilisateur voit les détails de sa commande")
+    public void user_sees_order_details() {
+        System.out.println("Détails de la commande affichés.");
+    }
+
     @When("l'utilisateur tente de valider une commande qui n'existe pas")
     public void user_tries_to_validate_nonexistent_order() {
         System.out.println("Tentative de valider une commande inexistante.");
@@ -27,5 +32,10 @@ public class ValidOrder {
     @Then("une erreur est renvoyée indiquant que la commande n'existe pas")
     public void error_order_does_not_exist_is_returned() {
         System.out.println("Erreur : La commande n'existe pas.");
+    }
+
+    @Then("l'utilisateur est invité à revenir à la page d'accueil")
+    public void user_is_redirected_to_homepage() {
+        System.out.println("L'utilisateur est invité à revenir à la page d'accueil.");
     }
 }
--- src/test/resources/exercice7/ValidOrder.feature
+++ src/test/resources/exercice7/ValidOrder.feature
@@ -5,8 +5,10 @@ Feature: Passer une commande
     Given l'utilisateur accède au formulaire de commande
     When l'utilisateur valide la commande
     Then l'utilisateur reçoit une confirmation de commande
+    And l'utilisateur voit les détails de sa commande
 
   Scenario: Tentative de passation d'une commande inexistante
     Given l'utilisateur accède au formulaire de commande
     When l'utilisateur tente de valider une commande qui n'existe pas
     Then une erreur est renvoyée indiquant que la commande n'existe pas
+    And l'utilisateur est invité à revenir à la page d'accueil
